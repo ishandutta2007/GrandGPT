91e9ae5eb59bb209f349c384eb943fc039bd8c4a
ishan dutta <ishandutta2007@gmail.com>
2023-06-29 00:55:10 +0530

    versioning

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@ import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import manifest from './manifest.config'
+import { viteZip } from 'vite-plugin-zip-file';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), crx({ manifest })],
+  plugins: [tsconfigPaths(), react(), crx({ manifest }),
+    viteZip({
+      folderPath: '/Users/ishandutta2007/Documents/Projects/GrandGPT/dist',
+      outPath: '/Users/ishandutta2007/Documents/Projects/GrandGPT/zips',
+      // outPath: path.resolve(__dirname),
+      zipName: 'chromium.zip',
+      // enabled: env.NODE_ENV === 'production'? true: false
+    })
+  ],
   build: {
     rollupOptions: {
       input: ['app.html', 'sidepanel.html'],
